95ea5ec025f65c6a255b381e044476ccdf769cf0
zhengxl5566 <zhengxl5566@gmail.com>
2020-02-12 23:56:23 +0800

    各种日志框架

---
+++ slf4j/logtest/src/main/java/zhengxl/App.java
@@ -0,0 +1,21 @@
+package zhengxl;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("log test");
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ slf4j/logtest/src/test/java/zhengxl/AppTest.java
@@ -0,0 +1,25 @@
+package zhengxl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void test(){
+
+        System.out.println("hi");
+    }
+}
