91d579c62ee8b35e645d416caa6cbb91e9236f6d
zhengxl <zhengxl5566@gmail.com>
2020-08-06 23:09:23 +0800

    完成上传

---
+++ thymeleaf-demo/src/main/java/com/zhengxl/thymeleafdemo/controller/Upload.java
@@ -0,0 +1,46 @@
+package com.zhengxl.thymeleafdemo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+public class Upload {
+    private final Logger logger = LoggerFactory.getLogger(Upload.class);
+
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
+    @PostMapping("upload")
+    @ResponseBody
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "c:/tmp/";
+        System.out.println(UUID.randomUUID().toString());
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            logger.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            logger.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
+
+}
