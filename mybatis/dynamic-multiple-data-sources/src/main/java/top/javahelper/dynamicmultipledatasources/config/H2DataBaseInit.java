2f9f26c6e39d449f401b7c8c91f81cd7cf9c1729
郑晓龙 <zhengxl5566@gmail.com>
2021-12-02 17:19:29 +0800

    引入多数据源配置

--- mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/config/H2DataBaseInit.java
+++ mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/config/H2DataBaseInit.java
@@ -1,30 +1,36 @@
-package top.javahelper.dynamicmultipledatasources.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.annotation.PostConstruct;
-
-/**
- * @description:
- * @projectName:dynamic-multiple-data-sources
- * @see:top.javahelper.dynamicmultipledatasources.config
- * @author:郑晓龙
- * @createTime:2021/12/1 17:35
- * @version:1.0
- */
-@Configuration
-public class H2DataBaseInit {
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    @PostConstruct
-    public void init() {
-        jdbcTemplate.execute("CREATE TABLE user ("
-                + "id VARCHAR(50) NOT NULL AUTO_INCREMENT,"
-                + "name VARCHAR(50) NOT NULL COMMENT '姓名',"
-                + "status VARCHAR(50) NOT NULL COMMENT '用户状态',"
-                + "PRIMARY KEY (id))");
-    }
-}
+//package top.javahelper.dynamicmultipledatasources.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.autoconfigure.jdbc.JdbcProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//
+//import javax.annotation.PostConstruct;
+//import javax.sql.DataSource;
+//
+///**
+// * @description:
+// * @projectName:dynamic-multiple-data-sources
+// * @see:top.javahelper.dynamicmultipledatasources.config
+// * @author:郑晓龙
+// * @createTime:2021/12/1 17:35
+// * @version:1.0
+// */
+//@Configuration
+//public class H2DataBaseInit {
+//    @Bean
+//    public JdbcTemplate JdbcTemplate(DataSource dataSource){
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//        JdbcProperties.Template template = new JdbcProperties.Template();
+//        jdbcTemplate.setFetchSize(template.getFetchSize());
+//        jdbcTemplate.setMaxRows(template.getMaxRows());
+//        if (template.getQueryTimeout() != null) {
+//            jdbcTemplate.setQueryTimeout((int) template.getQueryTimeout().getSeconds());
+//        }
+//        return jdbcTemplate;
+//    }
+//
+//}
---
+++ mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/config/MultipleDataSourcesConfig.java
@@ -0,0 +1,46 @@
+package top.javahelper.dynamicmultipledatasources.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description:
+ * @projectName:dynamic-multiple-data-sources
+ * @see:top.javahelper.dynamicmultipledatasources.config
+ * @author:郑晓龙
+ * @createTime:2021/12/2 11:24
+ * @version:1.0
+ */
+@Configuration
+public class MultipleDataSourcesConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.first")
+    public DataSource firstDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.second")
+    public DataSource secondDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public MultipleDataSource dataSource() {
+        MultipleDataSource dynamicDataSource = new MultipleDataSource();
+        dynamicDataSource.setDefaultTargetDataSource(firstDataSource());
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("first", firstDataSource());
+        dataSourceMap.put("second", secondDataSource());
+        dynamicDataSource.setTargetDataSources(dataSourceMap);
+        return dynamicDataSource;
+    }
+}
