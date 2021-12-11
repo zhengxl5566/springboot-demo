b8795d478b0d6c8dfa20f2543dd6ae38ee18e5c8
郑晓龙 <zhengxl5566@gmail.com>
2021-12-11 12:02:21 +0800

    数据源切面增加注释

--- mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/aspect/DataSourceAspect.java
+++ mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/aspect/DataSourceAspect.java
@@ -1,11 +1,11 @@
 package top.javahelper.dynamicmultipledatasources.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import top.javahelper.dynamicmultipledatasources.common.RoutingDataSourceContext;
 import top.javahelper.dynamicmultipledatasources.common.WithDataSource;
 
@@ -17,17 +17,24 @@ import top.javahelper.dynamicmultipledatasources.common.WithDataSource;
  * @createTime:2021/12/7 15:49
  * @version:1.0
  */
-@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Aspect
+@Component
 public class DataSourceAspect{
     @Around("@annotation(withDataSource)")
     public Object switchDataSource(ProceedingJoinPoint pjp, WithDataSource withDataSource) throws Throwable {
 
+        // 1.获取 @WithDataSource 注解中指定的数据源
         String routingKey = withDataSource.value();
+        // 2.设置数据源上下文
         RoutingDataSourceContext.setRoutingKey(routingKey);
+        // 3.使用设定好的数据源处理业务
         Object result = pjp.proceed();
+        // 4.清空数据源上下文
         RoutingDataSourceContext.reset();
+
         return result;
     }
 
 }
+
