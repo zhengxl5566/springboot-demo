a5427794b870f7a6d84da4d5e0e547ec812a54f3
郑晓龙 <zhengxl5566@gmail.com>
2022-05-25 16:50:11 +0800

    去掉无用注解

--- mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/DynamicMultipleDataSourcesApplication.java
+++ mybatis/dynamic-multiple-data-sources/src/main/java/top/javahelper/dynamicmultipledatasources/DynamicMultipleDataSourcesApplication.java
@@ -3,10 +3,8 @@ package top.javahelper.dynamicmultipledatasources;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 @MapperScan(value = "top.javahelper.dynamicmultipledatasources.mapper")
 public class DynamicMultipleDataSourcesApplication {
 
