f51d1bb32200488179fa398c25d4f45ff4ad6f88
fastbpmn <fastbpmn@gmail.com>
2022-07-07 16:18:21 +0800

    优化interceptor

--- interceptor-demo/src/main/java/com/zhengxl/interceptordemo/interceptor/HelloInterceptor.java
+++ interceptor-demo/src/main/java/com/zhengxl/interceptordemo/interceptor/HelloInterceptor.java
@@ -2,7 +2,7 @@ package com.zhengxl.interceptordemo.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,24 +17,24 @@ import javax.servlet.http.HttpServletResponse;
  * @createTime:2020/9/26 10:06
  * @version:1.0
  */
-@Component
-public class MyInterceptor implements HandlerInterceptor  {
-    private static Logger logger =  LoggerFactory.getLogger(MyInterceptor.class);
+@Configuration
+public class HelloInterceptor implements HandlerInterceptor  {
+    private static Logger logger =  LoggerFactory.getLogger(HelloInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        logger.info("MyInterceptor preHandle");
+        logger.info("HelloInterceptor ---> preHandle");
         return true;
 
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("MyInterceptor postHandle");
+        logger.info("HelloInterceptor ---> postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        logger.info("MyInterceptor afterCompletion");
+        logger.info("HelloInterceptor ---> afterCompletion");
     }
 }
--- interceptor-demo/src/main/java/com/zhengxl/interceptordemo/interceptor/HiInterceptor.java
+++ interceptor-demo/src/main/java/com/zhengxl/interceptordemo/interceptor/HiInterceptor.java
@@ -2,8 +2,8 @@ package com.zhengxl.interceptordemo.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,25 +18,25 @@ import javax.servlet.http.HttpServletResponse;
  * @createTime:2020/9/26 11:19
  * @version:1.0
  */
-@Component
-public class AnotherInterceptor implements HandlerInterceptor {
-    private static Logger logger = LoggerFactory.getLogger(AnotherInterceptor.class);
+@Configuration
+public class HiInterceptor implements HandlerInterceptor {
+    private static Logger logger = LoggerFactory.getLogger(HiInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        logger.info("AnotherInterceptor preHandle");
+        logger.info("HiInterceptor ---> preHandle");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
                            @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("AnotherInterceptor postHandle");
+        logger.info("HiInterceptor ---> postHandle");
     }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
                                  @Nullable Exception ex) throws Exception {
-        logger.info("AnotherInterceptor afterCompletion");
+        logger.info("HiInterceptor ---> afterCompletion");
     }
 }
